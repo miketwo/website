c1ade21fb6db4b55c5aec094ff0d2a2da0f7d86c
Michael Ricks-Aherne <miketwo@saucelabs.com>
2017-06-27 15:05:02 +0200

    Link to blog

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import webapp2
 application = webapp2.WSGIApplication([
     webapp2.Route('/learn/mocks', webapp2.RedirectHandler,
                   defaults={'_uri': 'https://youtu.be/smPbDqGjFAI'}),
+    webapp2.Route('/blog', webapp2.RedirectHandler,
+                  defaults={'_uri': 'https://miketwo.github.io/'})
 ], debug=False)
