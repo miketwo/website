276627bac55e2697da3b0554e3f0f2b91290d525
Michael Ricks-Aherne <miketwo@gmail.com>
2021-12-12 18:05:43 -0600

    update deplooy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 # You must already be logged in using gcloud auth login
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-appcfg.py -A miketwo-original update $DIR
+gcloud --project=miketwo-original app deploy $DIR
