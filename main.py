5ff4d4b78c50c6a7906703b2ee2992a014e5780a
Michael Aherne <michael.aherne@umbra.space>
2026-04-15 12:49:33 -0500

    Migrate App Engine deploy to Python 3

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import webapp2
+REDIRECTS = {
+    "/learn/mocks": "https://youtu.be/smPbDqGjFAI",
+    "/blog": "https://miketwo.github.io/",
+}
 
-application = webapp2.WSGIApplication([
-    webapp2.Route('/learn/mocks', webapp2.RedirectHandler,
-                  defaults={'_uri': 'https://youtu.be/smPbDqGjFAI'}),
-    webapp2.Route('/blog', webapp2.RedirectHandler,
-                  defaults={'_uri': 'https://miketwo.github.io/'})
-], debug=False)
+
+def app(environ, start_response):
+    path = environ.get("PATH_INFO", "")
+    destination = REDIRECTS.get(path)
+
+    if destination is None:
+        start_response(
+            "404 Not Found",
+            [("Content-Type", "text/plain; charset=utf-8")],
+        )
+        return [b"Not Found"]
+
+    start_response(
+        "302 Found",
+        [
+            ("Location", destination),
+            ("Content-Type", "text/plain; charset=utf-8"),
+        ],
+    )
+    return [b"Redirecting"]
